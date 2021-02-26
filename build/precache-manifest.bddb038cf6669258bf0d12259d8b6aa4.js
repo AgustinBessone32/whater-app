73ae3c9c7fcf771066e7e7f85732490dc51596cd
AgustinBessone32 <bessone98@gmail.com>
2021-02-26 17:15:57 -0300

    gh agregado

---
+++ build/precache-manifest.bddb038cf6669258bf0d12259d8b6aa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67f717ff96407bdda382a768315ef837",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00dea0a9b11430fbafe1",
+    "url": "/static/css/main.bc6ef857.chunk.css"
+  },
+  {
+    "revision": "41b7493613a0bd3f2249",
+    "url": "/static/js/2.5dab6682.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.5dab6682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00dea0a9b11430fbafe1",
+    "url": "/static/js/main.5e7c2ba8.chunk.js"
+  },
+  {
+    "revision": "16424b2c5fc1ee8e9e17",
+    "url": "/static/js/runtime-main.ca0f29c7.js"
+  }
+]);
