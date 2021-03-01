49d8f9be1fd380cd75964d510955849b22bdfc0a
Agustin Bessone <bessone98@gmail.com>
2021-03-01 01:09:33 -0300

    Updates

--- precache-manifest.2234012f0a2ae00a1a375f3020868dd6.js
+++ precache-manifest.2234012f0a2ae00a1a375f3020868dd6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b19f0bfdb251c8f35792aa31da103a8d",
+    "revision": "f451795ce4cfd0b9825edb5824c4f1f1",
     "url": "/whater-app/index.html"
   },
   {
-    "revision": "7b8e2dfa7d2474e7b0bf",
+    "revision": "38624da4ccb0e59cfbf2",
     "url": "/whater-app/static/css/main.91dd4a69.chunk.css"
   },
   {
-    "revision": "af1b8f1d318a75896d21",
-    "url": "/whater-app/static/js/2.c48c1ead.chunk.js"
+    "revision": "b9c856cddaeddf465878",
+    "url": "/whater-app/static/js/2.ed9ef41f.chunk.js"
   },
   {
     "revision": "f231859d6585c4cd5f80c344783ed269",
-    "url": "/whater-app/static/js/2.c48c1ead.chunk.js.LICENSE.txt"
+    "url": "/whater-app/static/js/2.ed9ef41f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7b8e2dfa7d2474e7b0bf",
-    "url": "/whater-app/static/js/main.1502eb70.chunk.js"
+    "revision": "38624da4ccb0e59cfbf2",
+    "url": "/whater-app/static/js/main.cb452f30.chunk.js"
   },
   {
     "revision": "f776614a5679855df82a",
