48f4f1e46e22e5d44c6c4b6c35346859aceb3bc2
AgustinBessone32 <bessone98@gmail.com>
2021-03-03 12:01:31 -0300

    Updates

---
+++ precache-manifest.598ba11e4060dbe447f6c1234cbc444a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24a97deac8678c50a4bf93be2b91e11",
+    "url": "/whater-app/index.html"
+  },
+  {
+    "revision": "1e771b4ddc0a37fdeb6c",
+    "url": "/whater-app/static/css/main.192b9217.chunk.css"
+  },
+  {
+    "revision": "f7686f8906a315684cfc",
+    "url": "/whater-app/static/js/2.959f400b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/whater-app/static/js/2.959f400b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e771b4ddc0a37fdeb6c",
+    "url": "/whater-app/static/js/main.2dbafb61.chunk.js"
+  },
+  {
+    "revision": "f776614a5679855df82a",
+    "url": "/whater-app/static/js/runtime-main.f96d3199.js"
+  }
+]);
